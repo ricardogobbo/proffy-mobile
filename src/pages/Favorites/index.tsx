a0acda109c7b7bb052fbc15d74e00ce4e0d5afe1
Ricardo Gobbo <rg.aguas@gmail.com>
2020-08-11 02:57:37 -0300

    feat: added Favorites page

---
+++ src/pages/Favorites/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+import "./styles";
+import PageHeader from "../../components/PageHeader";
+
+function Favorites() {
+  return (
+    <View>
+      <PageHeader title="Meus Proffys Favoritos" />
+    </View>
+  );
+}
+
+export default Favorites;
